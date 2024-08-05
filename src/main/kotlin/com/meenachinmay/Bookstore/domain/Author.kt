554e6dc46db02e9271f620b2241ea6c1608a9605
meenachinmay <chinmayanand896@gmail.com>
2024-08-06 00:55:55 +0900

    initial commit

---
+++ src/main/kotlin/com/meenachinmay/Bookstore/domain/Author.kt
@@ -0,0 +1,3 @@
+package com.meenachinmay.Bookstore.domain
+
+class Author(var id: Long?, var name: String, var age: Int, var description: String, var image: String)
---
+++ src/main/kotlin/com/meenachinmay/Bookstore/domain/Book.kt
@@ -0,0 +1,3 @@
+package com.meenachinmay.Bookstore.domain
+
+class Book(var isbn: Long, var title: String, var description: String, var image: String, var author: Author)
