d0bab8c5daf74055f9b7606b16656af3cc825f6d
meenachinmay <chinmayanand896@gmail.com>
2024-08-06 18:00:17 +0900

    author and book data classes, connected to database

--- src/main/kotlin/com/meenachinmay/Bookstore/domain/Book.kt
+++ src/main/kotlin/com/meenachinmay/Bookstore/domain/Book.kt
@@ -1,3 +1,24 @@
 package com.meenachinmay.Bookstore.domain
 
-class Book(var isbn: Long, var title: String, var description: String, var image: String, var author: Author)
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "books")
+data class Book(
+    @Id
+    @Column(name = "isbn")
+    var isbn: Long,
+
+    @Column(name = "title")
+    var title: String,
+
+    @Column(name = "description")
+    var description: String,
+
+   @Column(name = "image")
+    var image: String,
+
+    @ManyToOne(cascade = [(CascadeType.DETACH)])
+    @JoinColumn(name = "author")
+    var author: Author,
+)
